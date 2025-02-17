9a508830f32640ceaa037f3bfe02235a72a84b2a
olan <bee.olan@mail.ru>
2025-02-18 00:46:12 +0300

    Начало НашаТема

---
+++ manager/src/Controller/Proekt/BaseProController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Proekt;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BaseProController extends AbstractController
+{
+    /**
+     * @Route("/app/proekts/basepro", name="app.proekts.basepro")
+     * @return Response
+     */
+    public function basepro(): Response
+    {
+        $persona = "это я";
+        return $this->render('app/proekts/basepro.html.twig',
+         compact( 'persona')
+        );
+    }
+}
