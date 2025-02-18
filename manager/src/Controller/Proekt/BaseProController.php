2c0ace6d808f8df3743915bd1e14436e2893c993
olan <bee.olan@mail.ru>
2025-02-18 17:54:03 +0300

    удалила картинки

--- manager/src/Controller/Proekt/BaseProController.php
+++ manager/src/Controller/Proekt/BaseProController.php
@@ -4,7 +4,14 @@ declare(strict_types=1);
 
 namespace App\Controller\Proekt;
 
+use App\Model\Adminka\Entity\Uchasties\Personas\Id as PersonaId;
+use App\Model\Adminka\Entity\Uchasties\Personas\PersonaRepository;
+use App\Model\Adminka\Entity\Uchasties\Uchastie\Id;
+use App\Model\Adminka\Entity\Uchasties\Uchastie\UchastieRepository;
+use App\Model\Mesto\Entity\InfaMesto\Id as MestoNomerId;
+use App\Model\Mesto\Entity\InfaMesto\MestoNomerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,13 +19,25 @@ class BaseProController extends AbstractController
 {
     /**
      * @Route("/app/proekts/basepro", name="app.proekts.basepro")
+     * @param MestoNomerRepository $mestoNomers
+     * @param UchastieRepository $uchasties
+     * @param PersonaRepository $personas
      * @return Response
      */
-    public function basepro(): Response
+    public function index(Request $request,
+                          UchastieRepository $uchasties,
+                          PersonaRepository $personas,
+                          MestoNomerRepository $mestoNomers
+    ): Response
     {
-        $persona = "это я";
+        $idUser = $this->getUser()->getId();
+
+        $uchastie = $uchasties->has(new Id($idUser));
+        $persona = $personas->has(new PersonaId($idUser));
+        $mestoNomer = $mestoNomers->has(new MestoNomerId($idUser));
+
         return $this->render('app/proekts/basepro.html.twig',
-         compact( 'persona')
+         compact( 'uchastie', 'persona', 'mestoNomer')
         );
     }
 }
